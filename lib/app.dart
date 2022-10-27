07ec0ee4915a9f2f0f20bf241bf674f9bdb6b76e
Emirlan <Emirlanzh@mail.ru>
2022-10-27 11:32:00 +0600

    Smth

--- lib/app.dart
+++ lib/app.dart
@@ -12,15 +12,36 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Menu"),
-        ),
         body: Column(
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              color: Colors.red,
+          children: [FirstScreenBody()],
+        ),
+      ),
+    );
+  }
+}
+
+class FirstScreenBody extends StatelessWidget {
+  const FirstScreenBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: const Color.fromRGBO(148, 107, 236, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "САКТА",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 55,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.normal,
+                letterSpacing: 20,
+              ),
             )
           ],
         ),
